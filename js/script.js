0d00a8af98a74d9da2a9da6e8b945d539175d3c3
V1tale <elativ.c@gmail.com>
2022-10-14 14:42:00 +0200

    inizializzazione

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// PALINDROMA
+// INPUT
+// chiedo all'utente di inserire una parola
+// PROCESSING
+// confronto la parola con la stessa parola, ma di ordine inverso
+// se la parola dell'utente è uguale alla stessa parola di ordine inverso
+// OUTPUT
+// output positivo
+// altrimenti, output negativo
+
+// PARI E DISPARI
+// INPUT
+// chiedo all'utente di scegliere tra le parole "pari" e "dispari"
+// chiedo all'utente di scegliere un numero
+// PROCESSING
+// il computer genera un numero casuale
+// sommo il valore casuale del computer a quello scelto dall'utente
+// stabilisco criteri pari e dispari
+// se la somma numerica corrisponde al criterio pari o al criterio dispari
+// OUTPUT
+// output positivo
+// altrimenti, output negativo
